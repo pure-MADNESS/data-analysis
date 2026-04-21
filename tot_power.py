f152691c34bf342eeda2f53b53c4dda32d1d5ee3
alterlleo <leonardo.pasquato@gmail.com>
2026-04-21 16:23:30 +0200

    feat: required power from comuneditrento

---
+++ tot_power.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pymongo import MongoClient
+import os
+import numpy as np
+
+MONGO_URI = "mongodb://localhost:27017/"
+DB_NAME = "mads_socialist_4"
+FIELD_LOAD = 'message.request'
+FIELD_SOURCE = 'message.state.proposed_power'
+TIME_STAMP_COL = 'message.timestamp'
+
+def fetch_and_plot(start_time=None, end_time=None):
+    client = MongoClient(MONGO_URI)
+    db = client[DB_NAME]
+    
+    all_data_list = []
+
+    for topic in db.list_collection_names():
+        data = list(db[topic].find())
+        if not data: continue
+        
+        df = pd.json_normalize(data)
+        if TIME_STAMP_COL in df.columns:
+            df[TIME_STAMP_COL] = pd.to_datetime(df[TIME_STAMP_COL])
+            
+            # Filtro Timecode
+            time_col = 'message.timecode'
+            if time_col in df.columns:
+                df[time_col] = pd.to_numeric(df[time_col])
+                if start_time: df = df[df[time_col] >= start_time]
+                if end_time: df = df[df[time_col] <= end_time]
+            
+            if df.empty: continue
+            
+            cols = [TIME_STAMP_COL]
+            if FIELD_LOAD in df.columns: cols.append(FIELD_LOAD)
+            if FIELD_SOURCE in df.columns: cols.append(FIELD_SOURCE)
+            all_data_list.append(df[cols].copy())
+
+    if not all_data_list:
+        print("Nessun dato trovato.")
+        return
+
+    global_df = pd.concat(all_data_list, sort=False).sort_values(TIME_STAMP_COL)
+    global_df.set_index(TIME_STAMP_COL, inplace=True)
+
+    resampled = global_df.resample('10s').mean().fillna(0)
+
+
+    resampled['net_balance'] = resampled[FIELD_LOAD] - resampled[FIELD_SOURCE]
+
+    resampled['grid_import'] = resampled['net_balance'].apply(lambda x: x if x > 0 else 0)
+
+    hourly_report = resampled.resample('h').agg({
+        FIELD_LOAD: 'mean',      # Potenza media richiesta in quell'ora
+        FIELD_SOURCE: 'mean',    # Potenza media generata in quell'ora
+        'grid_import': 'sum'     # Somma dei prelievi "istantanei" dalla rete
+    })
+
+    print("\n" + "="*85)
+    print(f"{'TIME WINDOW':<20} | {'P. MEAN LOAD':<15} | {'P. MEAN GEN':<15} | {'PUBLIC CURRENT':<15}")
+    print("-" * 85)
+    
+    for index, row in hourly_report.iterrows():
+        if row[FIELD_LOAD] > 0 or row[FIELD_SOURCE] > 0:
+            print(f"{str(index.strftime('%H:%M')):<20} | {row[FIELD_LOAD]:>15.2f} | {row[FIELD_SOURCE]:>15.2f} | {row['grid_import']:>15.2f}")
+
+    print("="*85)
+
+if __name__ == "__main__":
+    fetch_and_plot(start_time=40125, end_time=40242)
